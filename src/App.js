5d4dc98800850f8560d522059478356031c4d4b7
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-08-08 14:35:03 +0530

    link updated HashRouter

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import HomeBody from "./components/HomeBody";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Project from "./components/Project";
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Switch>
+      <HashRouter>
+        <Routes>
           <Route path="/My-Website" element={<HomeBody />} />
           <Route path="/My-Website/about" element={<About />} />
           <Route path="/My-Website/project" element={<Project />} />
-        </Switch>
-      </BrowserRouter>
+        </Routes>
+      </HashRouter>
     </>
   );
 }
