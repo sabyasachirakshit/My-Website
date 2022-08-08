c794b59ecb60c7a9359e3b2d453ab1976473cf8b
Sabyasachi Rakshit <sabyasachirakshitacs19@gmail.com>
2022-08-08 14:32:04 +0530

    link updated 2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import HomeBody from "./components/HomeBody";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import About from "./components/About";
 import Project from "./components/Project";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Routes>
+        <Switch>
           <Route path="/My-Website" element={<HomeBody />} />
           <Route path="/My-Website/about" element={<About />} />
           <Route path="/My-Website/project" element={<Project />} />
-        </Routes>
+        </Switch>
       </BrowserRouter>
     </>
   );
